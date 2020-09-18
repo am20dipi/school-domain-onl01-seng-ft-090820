ac376eb91dde63b8ed84a3ad6425ca1289caa07a
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-18 15:30:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_reader :roster #getter
+  
+  def initialize(name)
+    @roster = {} #"roster should be an empty hash upon initialization"
+  end
+  
+  def add_student(name, grade) #instance method
+    @roster[grade] = []  if !@roster[grade] #set it equal to an empty array if the roster is nil 
+    @roster[grade] << name
+  end
+  
+  def grade(grade) #instance method
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade, names|
+      names.sort! #to make sort destructive & return the new array alphabetically
+    end
+  end
+end
+
+
+#school = School.new("Bayside High School")
+#school.roster = {}
