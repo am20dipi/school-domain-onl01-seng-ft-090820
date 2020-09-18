74d941758940c5c0f4b1a24cab54831b113a2459
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-18 15:01:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :grade
+  
+school = School.new("Bayside High School")
+school.roster = {}
+school.add_student("Zach Morris", 9)
+
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_student
+    self.class.all << self
+  end
+  
+  def sort
+    roster.sort
+  end
+end
+
+
+#school = School.new("Bayside High School")
+#school.roster = {}
